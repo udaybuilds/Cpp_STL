7cb4aa5117cd834832d0afb09ab9ec853acd38ba
udaybuilds <52237478+udaybuilds@users.noreply.github.com>
2022-12-09 01:02:20 +0530

    Array Code added

---
+++ Array.cpp
@@ -0,0 +1,15 @@
+#include <array>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int basic[3]={1,2,3};
+
+    array<int,4> a={1,2,3,4};
+
+    cout<<"Size of array is "<<a.size()<<endl;
+    cout<<"Element at second index "<<a.at(2)<< endl;
+    cout<<"Empty or not "<<a.empty()<<endl;
+    cout<<"Front Element "<<a.front()<<endl<<"Last Element "<<a.back();
+    return 1;
+}
