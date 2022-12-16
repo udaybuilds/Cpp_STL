7f4ebae62e5c776381da8f61ee598a0aad3e788f
udaybuilds <52237478+udaybuilds@users.noreply.github.com>
2022-12-16 22:56:09 +0530

    Quesue Code updated

--- Queue.cpp
+++ Queue.cpp
@@ -10,4 +10,10 @@ int main()
     q.push("Brainy");
     q.push("Srivastava");
     cout<<"Front String is "<<q.front()<<endl; //Last In Last Out 
+    cout<<"Back String is "<<q.back()<<endl;
+    q.pop();
+    cout<<"Front String after pop is "<<q.front()<<endl; 
+    cout<<"SIze after pop "<<q.size()<<endl;
+    //empty method also works
+    //all functions have O(1) complexity 
 }
